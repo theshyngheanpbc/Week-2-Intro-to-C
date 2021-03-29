8f2273d40eaabbf4be4f07f7f4f2f82c8b9c4f5b
Nguyen Hung Tien <81542088+theshyngheanpbc@users.noreply.github.com>
2021-03-29 10:20:10 +0700

    Add files via upload

---
+++ Week 2 exercise 2.3.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+	printf("Welcome to C\n");
+	printf("Progamming Introduction.\n");
+	return 0;
+}
---
+++ Week 2 exercise 2.5.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+	printf("My name is Hung Tien.\n");
+	printf("Nice to meet you.\n");
+	printf("Hope you will have happy time");
+	return 0;
+}
